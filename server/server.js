d1b83906e18d2e43b12e8adb0998e31042e77a42
Hitesh Aswani <hitesh.aswani@infostretch.com>
2017-08-25 11:20:54 +0530

    Setting up Socket.IO

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
 const publicPath = path.join(__dirname + '/../public');
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("User Connected");
+    console.log(socket);
+})
+
+server.listen(port, function(){
     console.log("Server is up on port: " + port);
 });
